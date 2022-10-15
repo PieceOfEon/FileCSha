966869cab9562fce77932092ec3584849ca6d2fd
PieceOfEon <danilskripka1996@gmail.com>
2022-10-15 14:13:05 +0300

    End

--- Program.cs
+++ Program.cs
@@ -1,35 +1,47 @@
-﻿Random Rand = new Random();
-string path = @"D:\GameTest5";
-DirectoryInfo dirInfo = new DirectoryInfo(path);
-string subpath = @"program";
-if (!dirInfo.Exists)
+﻿string directory = @"D:\TestTG";
+foreach (var fi in new DirectoryInfo(directory).EnumerateFiles("*.txt*", SearchOption.AllDirectories))
 {
-    dirInfo.Create();
-    Console.WriteLine("Успех");
-}
-else
-{
-    Console.WriteLine("Провал");
-}
-dirInfo.CreateSubdirectory(subpath);
-for (int i = 0; i < 100; i++)
-{
-    int q;
-    q = Rand.Next(0, 2);
-    int a;
-    a = Rand.Next(0,11);
-    if(a==0)
+    string folderDate = "ForTXT";
+    string newPath = directory + "\\" + folderDate;
+ 
+    if (!Directory.Exists(newPath))
     {
-        break;
+        Directory.CreateDirectory(newPath);
+        Console.WriteLine("Была создана папка: {0}", newPath);
     }
-    if(a<=9 && i>1)
-    {
-        if(q==1)
-        {
-            FileInfo f = new FileInfo(path+@"\"+subpath + @"\HELLO.txt");
-            FileStream fs = f.Create();
-        }
-        subpath += @"\program";
-        dirInfo.CreateSubdirectory(subpath);
-    }
+
+    newPath += "\\" + fi.Name;
+    File.Move(fi.FullName, newPath);
+    Console.WriteLine("Файл {0}, был перемещен в папку {1}", fi.FullName, newPath);
 }
+foreach (var fi in new DirectoryInfo(directory).EnumerateFiles("*.rtf*", SearchOption.AllDirectories))
+{
+    string folderDate2 = "ForRTF";
+    string newPath2 = directory + "\\" + folderDate2;
+
+    if (!Directory.Exists(newPath2))
+    {
+        Directory.CreateDirectory(newPath2);
+        Console.WriteLine("Была создана папка: {0}", newPath2);
+    }
+
+    newPath2 += "\\" + fi.Name;
+    File.Move(fi.FullName, newPath2);
+    Console.WriteLine("Файл {0}, был перемещен в папку {1}", fi.FullName, newPath2);
+}
+foreach (var fi in new DirectoryInfo(directory).EnumerateFiles("*.bmp*", SearchOption.AllDirectories))
+{
+    string folderDate3 = "ForBMP";
+    string newPath3 = directory + "\\" + folderDate3;
+
+    if (!Directory.Exists(newPath3))
+    {
+        Directory.CreateDirectory(newPath3);
+        Console.WriteLine("Была создана папка: {0}", newPath3);
+    }
+
+    newPath3 += "\\" + fi.Name;
+    File.Move(fi.FullName, newPath3);
+    Console.WriteLine("Файл {0}, был перемещен в папку {1}", fi.FullName, newPath3);
+}
+
